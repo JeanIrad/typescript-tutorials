f2fb2132e51fadc9d7422eeed2241155cea91271
Jean de Dieu <jado.milton@gmail.com>
2023-12-18 22:43:06 +0200

    first commit of type face

---
+++ build/js/main.js
@@ -0,0 +1,11 @@
+"use strict";
+let myName;
+let myAge;
+let address;
+myName = "Jean de Dieu";
+myAge = 21;
+address = {
+    email: "jado.milton@gmail.com",
+    phone: 787308777,
+    country_code: "+250",
+};
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+let myName: string;
+let myAge: number;
+let address: any;
+
+myName = "Jean de Dieu";
+myAge = 21;
+address = {
+  email: "jado.milton@gmail.com",
+  phone: 787308777,
+  country_code: "+250",
+};
