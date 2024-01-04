80f5f64316432fcc29fa3e95db30e9993e8cb0a8
Jean de Dieu <jado.milton@gmail.com>
2024-01-04 07:50:06 +0200

    committing basics
    functions, types, interfaces and many more

--- build/js/main.js
+++ build/js/main.js
@@ -2,10 +2,117 @@
 let myName;
 let myAge;
 let address;
+let myBirthdate;
 myName = "Jean de Dieu";
 myAge = 21;
 address = {
     email: "jado.milton@gmail.com",
     phone: 787308777,
     country_code: "+250",
+    street: "KK 83st",
+};
+myBirthdate = new Date("02-22-2000");
+console.log(myBirthdate.toLocaleDateString());
+// functions
+const sum = (a, b) => a + b;
+let postId;
+let guitar = ["Strat", "Les Paul"];
+let person = [
+    {
+        name: "some name",
+        // callName: function () {
+        //   return this.name;
+        // },
+    },
+    {
+        name: "say something son",
+    },
+    ["this"],
+];
+// tuples
+let myTuple = ["Jean", 23, true];
+let evh = {
+    name: "Eddie",
+    active: true,
+    albums: [1984, 5150, "OU812"],
+};
+let jp = {
+    name: "Jimmy",
+    active: false,
+    albums: ["I", "II", "IV"],
+};
+const greetGuitarist = (guitarist) => {
+    return `Hello ${guitarist.name}!`;
+};
+console.log(greetGuitarist(jp));
+// enums
+var Grade;
+(function (Grade) {
+    Grade[Grade["U"] = 1] = "U";
+    Grade[Grade["D"] = 2] = "D";
+    Grade[Grade["C"] = 3] = "C";
+    Grade[Grade["B"] = 4] = "B";
+    Grade[Grade["A"] = 5] = "A";
+})(Grade || (Grade = {}));
+// literal types
+let someName;
+let username;
+// functions
+const add = (a, b) => {
+    return a + b;
+};
+const logMessage = (message) => {
+    console.log(message);
+};
+logMessage("Hello");
+logMessage(add(43, 56));
+// logMessage('Hello')
+let subtract = function (c, d) {
+    return c - d;
+};
+// interface mathFunction {
+//   (a: number, b: number): number;
+// }
+let multiply = (a, b) => a * b;
+// logMessage(multiply(43, 21));
+// optional parameter
+const addAll = (a, b, c) => {
+    if (typeof c !== "undefined") {
+        return a + b + c;
+    }
+    return a + b;
+};
+// default value
+const sumAll = (a, b, c = 2) => {
+    return a + b + c;
+};
+// fs
+// const fs = require('fs')
+// Rest paramaters
+const total = (a, ...nums) => {
+    return a + nums.reduce((a, b) => a + b, 0);
+};
+logMessage(total(1, 2, 3, 4, 5, 8));
+const createError = (errMsg) => {
+    throw new Error(errMsg);
+};
+const infinite = () => {
+    let i = 1;
+    while (true) {
+        i++;
+        if (i > 100)
+            break;
+    }
+};
+// the use of the never type
+const numberOrString = (val) => {
+    if (typeof val === "string")
+        return "string";
+    if (typeof val === "number")
+        return "number";
+    return createError("this should not Happen");
+};
+// custom type guard
+const isNumber = (val) => {
+    return typeof val === "number" ? true : false;
 };
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 let myName: string;
 let myAge: number;
 let address: any;
+let myBirthdate: Date;
 
 myName = "Jean de Dieu";
 myAge = 21;
@@ -8,4 +9,143 @@ address = {
   email: "jado.milton@gmail.com",
   phone: 787308777,
   country_code: "+250",
+  street: "KK 83st",
+};
+
+myBirthdate = new Date("02-22-2000");
+console.log(myBirthdate.toLocaleDateString());
+
+// functions
+
+const sum = (a: number, b: number) => a + b;
+let postId: number | string;
+let guitar: string[] = ["Strat", "Les Paul"];
+let person: object[] = [
+  {
+    name: "some name",
+    // callName: function () {
+    //   return this.name;
+    // },
+  },
+  {
+    name: "say something son",
+  },
+  ["this"],
+];
+
+// tuples
+
+let myTuple: [string, number, boolean] = ["Jean", 23, true];
+
+// interfaces and types
+
+interface Guitarist {
+  name: string;
+  active?: boolean;
+  albums: (string | number)[];
+}
+let evh: Guitarist = {
+  name: "Eddie",
+  active: true,
+  albums: [1984, 5150, "OU812"],
+};
+let jp: Guitarist = {
+  name: "Jimmy",
+  active: false,
+  albums: ["I", "II", "IV"],
+};
+
+const greetGuitarist = (guitarist: Guitarist) => {
+  return `Hello ${guitarist.name}!`;
+};
+console.log(greetGuitarist(jp));
+// enums
+
+enum Grade {
+  U = 1,
+  D,
+  C,
+  B,
+  A,
+}
+// console.log(Grade);
+
+// type Aliases
+
+type stringOrNumber = string | number;
+type stringOrNumberArray = (string | number)[];
+
+type Guitarists = {
+  name: string;
+  active?: boolean;
+  albums: stringOrNumberArray;
+};
+type userId = stringOrNumber;
+
+// literal types
+
+let someName: "John";
+let username: "Dave" | "John" | "Amy";
+// functions
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+const logMessage = (message: any): void => {
+  console.log(message);
+};
+logMessage("Hello");
+logMessage(add(43, 56));
+// logMessage('Hello')
+let subtract = function (c: number, d: number): number {
+  return c - d;
+};
+type mathFunction = (a: number, b: number) => number;
+// interface mathFunction {
+//   (a: number, b: number): number;
+// }
+
+let multiply: mathFunction = (a: number, b: number) => a * b;
+
+// logMessage(multiply(43, 21));
+// optional parameter
+
+const addAll = (a: number, b: number, c?: number): number => {
+  if (typeof c !== "undefined") {
+    return a + b + c;
+  }
+
+  return a + b;
+};
+// default value
+const sumAll = (a: number, b: number, c: number = 2): number => {
+  return a + b + c;
+};
+
+// fs
+// const fs = require('fs')
+// Rest paramaters
+
+const total = (a: number, ...nums: number[]): number => {
+  return a + nums.reduce((a, b) => a + b, 0);
+};
+logMessage(total(1, 2, 3, 4, 5, 8));
+const createError = (errMsg: string): never => {
+  throw new Error(errMsg);
+};
+const infinite = () => {
+  let i: number = 1;
+  while (true) {
+    i++;
+    if (i > 100) break;
+  }
+};
+// the use of the never type
+const numberOrString = (val: number | string): string => {
+  if (typeof val === "string") return "string";
+  if (typeof val === "number") return "number";
+  return createError("this should not Happen");
+};
+// custom type guard
+const isNumber = (val: number): boolean => {
+  return typeof val === "number" ? true : false;
 };
