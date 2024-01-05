1480207699443b3425026dc5a2e3e6c240163d85
elie-2002 <imanirarutaelie@gmail.com>
2024-01-05 09:33:54 +0200

    commiting typescript classes

--- build/js/main.js
+++ build/js/main.js
@@ -134,5 +134,87 @@ let nextVal = addOrconcat(2, 4, "add");
 const img = document.querySelector("img"); // Not null Assertion
 const myImage = document.getElementById("myImage");
 const nextImg = document.querySelector("img");
-img.src;
-myImage.src;
+// img.src;
+// myImage.src;
+// classes
+class Coder {
+    constructor(name, music, age, lang = "Typescript") {
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+    }
+    getAge() {
+        return ` Hello, I am ${this.age} years old!`;
+    }
+}
+const jean = new Coder("Jean", "Rock", 21);
+console.log(jean.getAge());
+class webDev extends Coder {
+    constructor(computer, name, music, age) {
+        super(name, music, age);
+        this.computer = computer;
+        this.computer = computer;
+    }
+    getLang() {
+        return `I write ${this.lang}`;
+    }
+}
+const elie = new webDev("DELL", "Elie", "Lofi", 20);
+console.log(elie.getLang());
+class Guitarst {
+    constructor(name, instrument) {
+        this.name = name;
+        this.instrument = instrument;
+    }
+    play(action) {
+        return `${this.name} ${action} the ${this.instrument}`;
+    }
+}
+const Page = new Guitarst("Jimmy", "guitar");
+console.log(Page.play("strums"));
+// //////////////////////////
+class Peeps {
+    static getCount() {
+        return Peeps.count;
+    }
+    constructor(name) {
+        this.name = name;
+        this.name = name;
+        this.id = ++Peeps.count;
+    }
+}
+Peeps.count = 0;
+const John = new Peeps("John");
+const Steve = new Peeps("Steve");
+const Amy = new Peeps("Amy");
+const Brave = new Peeps("Brave");
+console.log(John);
+console.log(Peeps.count);
+///////////////////////////////////// getters and setters
+class Bands {
+    constructor() {
+        this.datastate = [];
+    }
+    get data() {
+        return this.datastate;
+    }
+    set data(value) {
+        if (Array.isArray(value) && value.every((el) => typeof el === "string")) {
+            this.datastate = value;
+            return;
+        }
+        else {
+            throw new Error("Params is not an array of strings");
+        }
+    }
+}
+const MyBands = new Bands();
+MyBands.data = ["Jean", "Loppez"];
+console.log(MyBands.data);
+MyBands.data = [...MyBands.data, "Peter", "Kamali"];
+console.log(MyBands.data);
--- src/main.ts
+++ src/main.ts
@@ -180,6 +180,116 @@ const img = document.querySelector("img")!; // Not null Assertion
 const myImage = document.getElementById("myImage") as HTMLImageElement;
 const nextImg = <HTMLImageElement>document.querySelector("img");
 
-img.src;
+// img.src;
 
-myImage.src;
+// myImage.src;
+// classes
+
+class Coder {
+  secondLang!: string; // a property that does not need to be instantiated
+  constructor(
+    public readonly name: string,
+    public music: string,
+    private age: number,
+    protected lang: string = "Typescript"
+  ) {
+    this.name = name;
+    this.music = music;
+    this.age = age;
+    this.lang = lang;
+  }
+  public getAge() {
+    return ` Hello, I am ${this.age} years old!`;
+  }
+}
+
+const jean = new Coder("Jean", "Rock", 21);
+console.log(jean.getAge());
+
+class webDev extends Coder {
+  constructor(
+    public computer: string,
+    name: string,
+    music: string,
+    age: number
+  ) {
+    super(name, music, age);
+    this.computer = computer;
+  }
+  public getLang() {
+    return `I write ${this.lang}`;
+  }
+}
+
+const elie = new webDev("DELL", "Elie", "Lofi", 20);
+console.log(elie.getLang());
+// console.log(elie);
+///////////////////////////////////////////////////
+
+interface Musician {
+  name: string;
+  instrument: string;
+  play(action: string): string;
+}
+
+class Guitarst implements Musician {
+  name: string;
+  instrument: string;
+  constructor(name: string, instrument: string) {
+    this.name = name;
+    this.instrument = instrument;
+  }
+  play(action: string): string {
+    return `${this.name} ${action} the ${this.instrument}`;
+  }
+}
+const Page = new Guitarst("Jimmy", "guitar");
+
+console.log(Page.play("strums"));
+
+// //////////////////////////
+
+class Peeps {
+  static count: number = 0;
+  static getCount(): number {
+    return Peeps.count;
+  }
+  public id: number;
+  constructor(public name: string) {
+    this.name = name;
+    this.id = ++Peeps.count;
+  }
+}
+
+const John = new Peeps("John");
+const Steve = new Peeps("Steve");
+const Amy = new Peeps("Amy");
+const Brave = new Peeps("Brave");
+console.log(John);
+console.log(Peeps.count);
+
+///////////////////////////////////// getters and setters
+
+class Bands {
+  private datastate: string[];
+  constructor() {
+    this.datastate = [];
+  }
+  public get data(): string[] {
+    return this.datastate;
+  }
+  public set data(value: string[]) {
+    if (Array.isArray(value) && value.every((el) => typeof el === "string")) {
+      this.datastate = value;
+      return;
+    } else {
+      throw new Error("Params is not an array of strings");
+    }
+  }
+}
+
+const MyBands = new Bands();
+MyBands.data = ["Jean", "Loppez"];
+console.log(MyBands.data);
+MyBands.data = [...MyBands.data, "Peter", "Kamali"];
+console.log(MyBands.data);
